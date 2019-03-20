13e504938a4a3960f939194a18b4486564e09847
Mckay <mckaywrigley@gmail.com>
2019-03-20 09:23:17 -0700

    adds starter data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import './App.css';
 import TodoList from './components/TodoComponents/TodoList.js';
 import TodoForm from './components/TodoComponents/TodoForm.js';
 
+const todoData = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
